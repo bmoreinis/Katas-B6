daab95f500d981cbf242e888f3dfd534c493bdf5
Bram Moreinis <bmoreinis@gmail.com>
2021-02-26 14:38:23 +0000

    added conditionals

--- script.js
+++ script.js
@@ -10,6 +10,11 @@
 // make a string variable for color, prompt user for it
 // alert color " is a nice color!"
 
+/* Conditional */
+// make a string variable for color, prompt user for it
+// if user entere "black" alert "Black is not really a color." 
+// otherwise alert color " is a nice color!"
+
 /* AGE KATAS */
 
 /* No Prompt */
@@ -22,25 +27,36 @@
 // make a string variable for name, prompt user for it
 // alert name is age years old
 
+/* Prompt with Conditional */
+// make a number variable for age, prompt user for it
+// make a string variable for name, prompt user for it
+// if age is greater than 16, alert "you don't look that old!"
+// otherwise alert name is age years old
+
 /* ICE CREAM KATAS */
-
-/* No Prompt */
-
- 
 
 /* Prompt */
 // make a variable for ice cream flavor, prompt user for it
 // make a variable for scoops, prompt user for it
 // alert "You want " scoops "scoops of " flavor
 
-/* PET KATAS */
+/* Prompt with Conditional*/
+// make a variable for ice cream flavor, prompt user for it
+// make a variable for scoops, prompt user for it
+// if scoop is creater than three, alert ("Max 3 scoops!")
+// otherwise alert "You want " scoops "scoops of " flavor
 
-/* No Prompt */
-// variable for a pet type 
-// variable for a pet name
-// alert my pet type  is named pet name
+/* PET KATAS */
 
 /* Prompt */
 // make a variable for pet type, prompt user for it 
 // make a variable for pet name, prompt user for it
 // alert "You have a pet type named pet name 
+
+/* Prompt with Conditionals */
+// make a variable for pet type, prompt user for it 
+// make a variable for pet name, prompt user for it
+// alert "You have a pet type named pet name 
+// if pet is a dog, say "I like dogs, too!"
+// if it is a cat, say "I'm allergic to cats"
+// If it is not a dog or a cat, say "what an interesting pet!"
